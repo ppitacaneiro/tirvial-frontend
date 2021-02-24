d74d8a09523fa48a98136edda1062355e8125650
pablo <ppitacaneiro@gmail.com>
2021-02-24 19:03:58 +0100

    Nombre de jugador y categoría

--- src/app/models/ranking.model.ts
+++ src/app/models/ranking.model.ts
@@ -2,7 +2,9 @@ export class Ranking {
 
     id : number;
     player_id : number;
+    player_name : string;
     category_id : number;
+    category_name : string;
     hits : number;
     errors : number;
     score : number;
